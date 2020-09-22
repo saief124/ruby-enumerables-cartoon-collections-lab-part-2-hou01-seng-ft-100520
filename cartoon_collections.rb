e1034afd1057355667011779845076e301c5b966
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-22 05:59:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   array.capitalize do |element|
     element+"!"
+  end
 end
 
 def long_planeteer_calls(planeteer_calls)
